ceaf84ea0e0860117d819aa090eaf1857f05e888
marithawang <wang.maritha@gmail.com>
2014-04-09 12:50:09 -0700

    Update Main.java

--- DNA/src/Main.java
+++ DNA/src/Main.java
@@ -1,4 +1,18 @@
 
 public class Main {
-
+	public static void main(String[] args){
+	
+	}
+	public int[] splice(int[] strand){
+		return strand;
+		
+	}
+	public int[] transcribe(int[] strand){
+		return strand;
+		
+	}
+	public int[] translate(int[] strand){
+		return strand;
+		
+	}
 }
