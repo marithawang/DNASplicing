90bb9793771ee43697b9b41713318ec305b8f472
marithawang <wang.maritha@gmail.com>
2014-04-09 18:58:29 -0700

    Update Main.java
    generates random DNA strand, takes use input for split location of strand
    also prints original DNA strand

--- DNA/src/Main.java
+++ DNA/src/Main.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args){
-	
+		int[] DNA = new int[20];
+		
+		System.out.println("Original DNA Sequence:");
+		for (int i = 0; i<DNA.length; i++){
+			DNA[i]=(int)(Math.random()*4);
+		}
+		toDNA(DNA);
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Sequence to Cut At (Indicate Split Position with Space):");
+		System.out.println("For Example: G AAT");
+		String input=scan.nextLine();
+		
+		splice(DNA, input);
 	}
-	public int[] splice(int[] strand){
+
+	public static int[] splice(int[] strand, String splitPos){
 		return strand;
 		
 	}
@@ -14,5 +29,23 @@ public class Main {
 	public int[] translate(int[] strand){
 		return strand;
 		
+	}
+	
+	public static void toDNA(int[] a){
+		
+		for(int i = 0; i<a.length; i++){
+			String nucleotide = "";
+			switch (a[i]){
+			case 0: nucleotide = "A";
+			break;
+			case 1: nucleotide = "T";
+			break;
+			case 2: nucleotide = "C";
+			break;
+			case 3: nucleotide = "G";
+			}
+			System.out.print(nucleotide);
+		}
+		System.out.println("");
 	}
 }
