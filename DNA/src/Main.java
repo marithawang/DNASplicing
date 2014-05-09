f348d77c01cebd434787d42759dad1a404fa6354
alexwong1 <masterwong1997@yahoo.com>
2014-05-09 09:23:37 -0700

    Update Main.java

--- DNA/src/Main.java
+++ DNA/src/Main.java
@@ -20,18 +20,21 @@ public class Main {
 			strand = strand.substring(0) + baseLet;
 			System.out.print(strand.charAt(i));
 		}
+//		
+//		System.out.println("");
+//		Scanner scan = new Scanner(System.in);
+//		System.out.println("");
+//		System.out.println("Choose Restriction Enzyme (Indicate Number):");
+//		System.out.println("1. EcoRI \n2. MscI \n3. PsiI");
+//		String input = scan.nextLine();
+//		System.out.println("Foreign Strand to Insert:");
+//		String insert = scan.nextLine();
+//
+
+	//	insert(input, insert, strand);
+		System.out.println("");
+		System.out.print(transcribe(strand));
 		
-		System.out.println("");
-		Scanner scan = new Scanner(System.in);
-		System.out.println("");
-		System.out.println("Choose Restriction Enzyme (Indicate Number):");
-		System.out.println("1. EcoRI \n2. MscI \n3. PsiI");
-		String input = scan.nextLine();
-		System.out.println("Foreign Strand to Insert:");
-		String insert = scan.nextLine();
-
-
-		insert(input, insert, strand);
 	}
 	
 	public static void insert(String input, String insert, String strand){
@@ -39,11 +42,34 @@ public class Main {
 		
 	}
 
-	public String transcribe(String strand){
-		return strand;
+	public static String transcribe(String strand){
+		ArrayList<String> s = new ArrayList<String>();
+		ArrayList<String> t = new ArrayList<String>();
+		
+		for (int i = 0; i < strand.length(); i++) {
+			s.add(strand.substring(i, i + 1));
+		}
+		
+		for (int j = 0; j < s.size(); j++) {
+			if (s.get(j).equals("A")) {
+				t.add("U");
+			} else if (s.get(j).equals("T")) {
+				t.add("A");
+			} else if (s.get(j).equals("C")) {
+				t.add("G");
+			} else {
+				t.add("C");
+			}
+		}
+		String transcribedStrand = "";
+		
+		for (int k = 0; k < t.size(); k++){
+			transcribedStrand = transcribedStrand + t.get(k);
+		}
+		return transcribedStrand;
 		
 	}
-	public String translate(String strand){
+	public static String translate(String strand){
 		return strand;
 		
 	}
